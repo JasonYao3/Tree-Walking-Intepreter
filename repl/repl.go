090dbd3cc21dc9f7938f3059bc29afd53a9d17b3
Jason Yao <jasonyao105025@gmail.com>
2024-01-23 21:43:54 -0500

    Extend the Parser

--- repl/repl.go
+++ repl/repl.go
@@ -4,13 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"go_interpreter/lexer"
-	"go_interpreter/token"
+	"go_interpreter/parser"
 	"io"
 )
 
 /*
 the REPL reads input, sends it to the interpreter for evaluation, prints the result/output of the
-interpreter and starts again. Read, Eval, Print, Loop.
+interpreter and starts again. Read, Parse, Print, Loop.
 */
 
 const PROMPT = ">> "
@@ -27,9 +27,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
